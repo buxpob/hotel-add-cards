45d27892249b98b6a0d6ba988764541625b681ea
buxpob <buxpob.xax@gmail.com>
2021-04-13 21:56:47 +0300

    commit

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,14 +1,43 @@
-const listButtons = document.querySelectorAll(".button");
-const listCards = document.querySelectorAll(".room-card__reserve");
-const listReserve = document.querySelectorAll(".room-card__reserve");
+const roomsList = document.querySelector(".rooms__list");
+const listButtons = roomsList.querySelectorAll(".room-card__button");
+const listReserve = roomsList.querySelectorAll(".room-card__reserve");
+const listReserveLink = roomsList.querySelectorAll(".room-card__payment-link");
+const listRoomLink = roomsList.querySelectorAll(".room-card__title-link");
 
-for (let i = 0; i < listButtons.length; i++) {
-   console.log(listButtons[i]);
-   listButtons[i].onclick = () => {
-      console.log('1');
-   }
+const list = roomsList.querySelectorAll(".room-card");
+
+const roomReserve = (el) => {
+   listReserve[el].classList.remove("room-card__reserve--current");
+   listButtons[el].tabIndex = -1;
+   listReserve[el].tabIndex = 0;
+   listReserveLink[el].tabIndex = 0;
+   listRoomLink[el].tabIndex = -1;
 }
 
-// button.onclick = () => {
-//    console.log("Hello");
-// }
+const roomCancelReservation = (el) => {
+   listReserve[el].classList.add("room-card__reserve--current");
+   listButtons[el].tabIndex = 0;
+   listReserve[el].tabIndex = -1;
+   listReserveLink[el].tabIndex = -1;
+   listRoomLink[el].tabIndex = 0;
+}
+
+for (let i = 0; i < listButtons.length; i++) {
+   listButtons[i].onclick = () => {
+      roomReserve(i);
+   };
+
+   listReserve[i].onclick = () => {
+      roomCancelReservation(i);
+   };
+
+   listReserve[i].focus = () => {
+      roomCancelReservation(i);
+   };
+
+   listReserve[i].onkeydown = (evt) => {
+      if (evt.key === ("Escape" || "Esc")) {
+         roomCancelReservation(i);
+      }
+   }
+}
